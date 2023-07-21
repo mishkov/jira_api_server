62426550ea2f87d7786cbe7af1f6e6b17264b962
Nikita Mishkov <mishkov.dd@gmail.com>
2023-07-22 00:54:56 +0300

    Add missing middleware

--- bin/server.dart
+++ bin/server.dart
@@ -177,7 +177,10 @@ void main(List<String> args) async {
   final ip = InternetAddress.anyIPv4;
 
   // Configure a pipeline that logs requests.
-  final handler = Pipeline().addMiddleware(logRequests()).addHandler(_router);
+  final handler = Pipeline()
+      .addMiddleware(corsMiddleware) // Add the CORS middleware here
+      .addMiddleware(logRequests())
+      .addHandler(_router);
 
   // For running in containers, we respect the PORT environment variable.
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
